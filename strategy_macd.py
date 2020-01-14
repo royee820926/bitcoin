4fc05ebaea27f4e308129023cbb8f6907ff85730
邓俊 <dengjun@dengjundeMacBook-Pro.local>
2020-01-15 00:29:30 +0800

    macd

--- strategy_macd.py
+++ strategy_macd.py
@@ -78,37 +78,72 @@ df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume']]
 df['ma7'] = df['close'].rolling(7, min_periods=1).mean()
 df['ma30'] = df['close'].rolling(30, min_periods=1).mean()
 
-# macd
-# df['ema12'] = df['close'].shift(1) + (df['close'] - df['close'].shift(1)) * 2 / (12 + 1)
-# df['ema26'] = df['close'].shift(1) + (df['close'] - df['close'].shift(1)) * 2 / (26 + 1)
-
 # 先复制第一行的ema12和ema26为收盘价（shift(1)：上一条）
-df['ema12'] = 0
-df['ema26'] = 0
-# 先初始化第一行ema12和ema26
-df.at[0, 'ema12'] = df.at[0, 'close']
-df.at[0, 'ema26'] = df.at[0, 'close']
+df['ema12'] = 0.0
+df['ema26'] = 0.0
+
 # 后面从第二行开始计算
-
 for index, row in df.iterrows():
-
     if index == 0:
-        prev_ema12 = row['ema12']
-        prev_ema26 = row['ema26']
+        # 先初始化第一行ema12和ema26
+        prev_ema12 = df.at[0, 'ema12'] = df.at[0, 'close']
+        prev_ema26 = df.at[0, 'ema26'] = df.at[0, 'close']
     else:
-        # todo
-        pass
-    exit()
+        prev_ema12 = df.at[index, 'ema12'] = (row['close'] * 2 + (12 - 1) * prev_ema12) / (12 + 1)
+        prev_ema26 = df.at[index, 'ema26'] = (row['close'] * 2 + (26 - 1) * prev_ema26) / (26 + 1)
 
 # df.loc[df['ema12'] == 0, 'ema12'] = (df['close'] * 2 + (12 - 1) * df['ema12'].shift(1)) / (12 + 1)
 # df.loc[df['ema26'] == 0, 'ema26'] = (df['close'] * 2 + (26 - 1) * df['ema26'].shift(1)) / (26 + 1)
-print(df[['candle_begin_time', 'close', 'ema12', 'ema26']])
-exit()
 
 df['diff']  = df['ema12'] - df['ema26']
 df['dea']   = df['diff'].rolling(9, min_periods=1).mean()
 # macd柱线
 df['macd_bar']  = (df['diff'] - df['dea']) * 2
-print(df[['candle_begin_time', 'close', 'ma7', 'ma30', 'ema12', 'ema26', 'diff', 'dea', 'macd_bar']])
+
+# 交易量移动平均线
+df['vma5']  = df['volume'].rolling(5, min_periods=1).mean()
+df['vma10'] = df['volume'].rolling(10, min_periods=1).mean()
+
+# print(df[['candle_begin_time', 'close', 'ma7', 'ma30', 'ema12', 'ema26', 'diff', 'dea', 'macd_bar', 'volume', 'vma5', 'vma10']])
+# exit()
+
+# ==== 标记信号 ====
+# 做多
+
+# 做多平仓
+
+# 做空
+
+# 做空平仓
+
+# ====合并做多做空信号，去除重复信号
+# df['signal'] = df[['signal_long', 'signal_short']].sum(axis=1)
+# 如果pandas版本最新，使用下面代码和视频保持一致。
+# min_count的意思是指定 NaN 个最少个数为1 超过1个NaN 就不计算 所以不会出现0.0
+# df['signal'] = df[['signal_long', 'signal_short']].sum(axis=1, min_count=1, skipna=True)
 
 
+# temp = df[df['signal'].notnull()][['signal']]
+# print(temp.iloc[0: 10])
+
+# temp = temp[temp['signal'] != temp['signal'].shift(1)]
+# print(temp.iloc[0: 10])
+# exit()
+# df['signal'] = temp['signal']
+# print(df[['candle_begin_time', 'signal']])
+# exit()
+
+# 删除中间列
+# df.drop(['median', 'std', 'upper', 'lower', 'signal_long', 'signal_short'], axis=1, inplace=True)
+
+# ====由signal计算出实际每天持有的仓位
+# signal的计算运用了收盘价，是每根K线收盘之后产生的信号，到第二根开盘的时候才买入，仓位才会改变。
+# df['pos'] = df['signal'].shift()
+# df['pos'].fillna(method='ffill', inplace=True)
+# df['pos'].fillna(value=0, inplace=True)     # 将初始行数的position补全为0
+
+# print(df)
+
+# ====将数据存入hdf文件中
+# df.to_hdf('output/eth_bolling_signal.h5',
+#           key='all_data', mode='w')
