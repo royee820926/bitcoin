7ce518ba250e09293fcec86bfde6136733721109
dengjunxp <263139762@qq.com>
2020-01-15 17:01:57 +0800

    macd

--- strategy_macd.py
+++ strategy_macd.py
@@ -10,11 +10,12 @@ pd.set_option('expand_frame_repr', False)
 # pd.set_option('display.min_rows', 500)
 pd.set_option('display.max_rows', None)
 
-instrument_id = 'BTC-USDT'
+# instrument_id = 'BTC-USDT'
+instrument_id = 'EOS-USDT'
 collection = get_spot_collection(instrument_id)
 
-# 取出多少条1分钟K线
-kline_length = 60 * 24
+# 取出多少条1分钟K线（选取2天的K线前导数据，diff和dea计算结果会趋于为固定值）
+kline_length = 2 * 24 * 60
 # 从数据库读取K线记录
 temp = collection.find().sort([('time', -1)]).limit(kline_length)
 result = []
@@ -59,7 +60,8 @@ df['candle_begin_time'] = pd.to_datetime(df['candle_begin_time'], format='%Y-%m-
 # print(type(df.iloc[0]['candle_begin_time']))
 # exit()
 # 重采样
-rule_type = '5T'
+kline_rule = 5
+rule_type = '%dT' % kline_rule
 
 period_df = df.resample(rule=rule_type, on='candle_begin_time', label='left', closed='left').agg({
     'open': 'first',
@@ -75,13 +77,11 @@ period_df.reset_index(inplace=True)
 df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume']]
 
 # 计算移动平均线
-df['ma7'] = df['close'].rolling(7, min_periods=1).mean()
+df['ma7']  = df['close'].rolling(7, min_periods=1).mean()
+df['ma20'] = df['close'].rolling(20, min_periods=1).mean()
 df['ma30'] = df['close'].rolling(30, min_periods=1).mean()
 
 # 先复制第一行的ema12和ema26为收盘价（shift(1)：上一条）
-df['ema12'] = 0.0
-df['ema26'] = 0.0
-
 # 后面从第二行开始计算
 for index, row in df.iterrows():
     if index == 0:
@@ -92,30 +92,70 @@ for index, row in df.iterrows():
         prev_ema12 = df.at[index, 'ema12'] = (row['close'] * 2 + (12 - 1) * prev_ema12) / (12 + 1)
         prev_ema26 = df.at[index, 'ema26'] = (row['close'] * 2 + (26 - 1) * prev_ema26) / (26 + 1)
 
-# df.loc[df['ema12'] == 0, 'ema12'] = (df['close'] * 2 + (12 - 1) * df['ema12'].shift(1)) / (12 + 1)
-# df.loc[df['ema26'] == 0, 'ema26'] = (df['close'] * 2 + (26 - 1) * df['ema26'].shift(1)) / (26 + 1)
-
-df['diff']  = df['ema12'] - df['ema26']
-df['dea']   = df['diff'].rolling(9, min_periods=1).mean()
+df['dif']  = df['ema12'] - df['ema26']
+df['dea']   = df['dif'].rolling(9, min_periods=1).mean()
 # macd柱线
-df['macd_bar']  = (df['diff'] - df['dea']) * 2
+df['macd_bar']  = (df['dif'] - df['dea']) * 2
 
 # 交易量移动平均线
-df['vma5']  = df['volume'].rolling(5, min_periods=1).mean()
-df['vma10'] = df['volume'].rolling(10, min_periods=1).mean()
+# df['vma5']  = df['volume'].rolling(5, min_periods=1).mean()
+# df['vma10'] = df['volume'].rolling(10, min_periods=1).mean()
 
-# print(df[['candle_begin_time', 'close', 'ma7', 'ma30', 'ema12', 'ema26', 'diff', 'dea', 'macd_bar', 'volume', 'vma5', 'vma10']])
+# BOLL指标
+n = 100     # 中轨n根K线的移动平均线
+m = 2       # 系数
+# 计算中轨
+df['median'] = df['close'].rolling(n, min_periods=1).mean()
+# 计算标准差
+df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof 标准差自由度
+# 计算上轨
+df['upper'] = df['median'] + m * df['std']
+# 计算下轨
+df['lower'] = df['median'] - m * df['std']
+print(df[['candle_begin_time', 'close', 'median', 'std', 'upper', 'lower']])
+exit()
+# RSI 指标
+# df['rsi1']
+# df['rsi2']
+# df['rsi3']
+
+# print(df.tail(40))
+# exit()
+
+# print(df[['candle_begin_time', 'close', 'ma7', 'ma30', 'ema12', 'ema26', 'dif', 'dea', 'macd_bar', 'volume', 'vma5', 'vma10']])
 # exit()
 
 # ==== 标记信号 ====
-# 做多
+# 做多信号
+# DIF上穿DEA
+dif_cond1 = df['dif'] > df['dea']
+dif_cond2 = df['dif'].shift(1) <= df['dea'].shift(1)
+# MACD由负变正
+bar_cond1 = df['macd_bar'] > df['macd_bar'].shift(1)
+bar_cond2 = df['macd_bar'] > 0
+bar_cond3 = df['macd_bar'].shift(1) <= 0
 
-# 做多平仓
+df.loc[dif_cond1 & dif_cond2 & bar_cond1 & bar_cond2 & bar_cond3 , 'signal'] = 1
 
+# 过滤虚假的做多信号
+# 往前推3个小时（如果是5分钟K线，就是36根K线），判断当前K线处在什么位置
+back_minutes = 180
+kline_length = back_minutes / kline_rule
+
+temp = df[df['signal'] == 1]
+for index, row in temp.iterrows():
+    if index < kline_length:
+        continue
+
+print(temp)
+exit()
 # 做空
 
+# 做多平仓
 # 做空平仓
 
+print(df)
+exit()
 # ====合并做多做空信号，去除重复信号
 # df['signal'] = df[['signal_long', 'signal_short']].sum(axis=1)
 # 如果pandas版本最新，使用下面代码和视频保持一致。
