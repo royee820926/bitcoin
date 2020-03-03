09dcfe922a29c6816d1bbd3cb1c98fe75905d0e9
dengjunxp <263139762@qq.com>
2020-03-03 17:38:58 +0800

    trade

--- strategy_macd.py
+++ strategy_macd.py
@@ -6,6 +6,7 @@ import pandas as pd
 import talib as ta
 import time
 
+
 # 不换行显示
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.max_rows', 100)
@@ -85,64 +86,34 @@ df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume']]
 # df['ma30'] = df['close'].rolling(30, min_periods=1).mean()
 # df['ma60'] = df['close'].rolling(60, min_periods=1).mean()
 
-# TA-Lib -> RSI
-# df['dif'], df['dea'], df['macd_bar'] = ta.MACD(df['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+# TA-Lib -> MACD
+df['dif'], df['dea'], df['macd_bar'] = ta.MACD(df['close'], fastperiod=12, slowperiod=26, signalperiod=9)
 
 # TA-Lib -> RSI
 df['rsi6'] = ta.RSI(df['close'], timeperiod=6)
 df['rsi12'] = ta.RSI(df['close'], timeperiod=12)
 df['rsi24'] = ta.RSI(df['close'], timeperiod=24)
-# df['fastk'], df['fastd'] = ta.STOCHRSI(df['close'], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)
-print(df)
+print(df.tail(50))
 exit()
-
-
-
 
 # 交易量移动平均线
 # df['vma5']  = df['volume'].rolling(5, min_periods=1).mean()
 # df['vma10'] = df['volume'].rolling(10, min_periods=1).mean()
 
-# BOLL指标
-n = 20     # 中轨n根K线的移动平均线
-m = 2      # 系数
-# 计算中轨
-df['median'] = df['close'].rolling(n, min_periods=1).mean()
-# 计算标准差
-df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof 标准差自由度
-# 计算上轨
-df['upper'] = df['median'] + m * df['std']
-# 计算下轨
-df['lower'] = df['median'] - m * df['std']
+# # BOLL指标
+# n = 20     # 中轨n根K线的移动平均线
+# m = 2      # 系数
+# # 计算中轨
+# df['median'] = df['close'].rolling(n, min_periods=1).mean()
+# # 计算标准差
+# df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof 标准差自由度
+# # 计算上轨
+# df['upper'] = df['median'] + m * df['std']
+# # 计算下轨
+# df['lower'] = df['median'] - m * df['std']
+
 # print(df[['candle_begin_time', 'close', 'median', 'std', 'upper', 'lower']])
-# exit()
 
-# OBV指标（能量潮）
-# for index, row in df.iterrows():
-#     if index == 0:
-#         prev_obv = df.at[0, 'obv'] = 0
-#     else:
-#         if df.at[index, 'close'] > prev_obv:
-#             prev_obv = df.at[index, 'obv'] = prev_obv + df.at[index, 'volume']
-#         elif df.at[index, 'close'] < prev_obv:
-#             prev_obv = df.at[index, 'obv'] = prev_obv - df.at[index, 'volume']
-#         else:
-#             prev_obv = df.at[index, 'obv'] = prev_obv
-# print(df.tail(40))
-# exit()
-
-# RSI指标
-# df['rsi1']
-# df['rsi2']
-# df['rsi3']
-
-# KDJ指标
-
-# print(df.tail(40))
-# exit()
-
-# print(df[['candle_begin_time', 'close', 'ma7', 'ma30', 'ema12', 'ema26', 'dif', 'dea', 'macd_bar', 'volume', 'vma5', 'vma10']])
-# exit()
 
 # ==== 标记信号 ====
 # [2：做多、1：做多平仓、-2：做空、-1：做空平仓]
@@ -171,47 +142,6 @@ df['lower'] = df['median'] - m * df['std']
 # line_cond1 = df['close'] < df['ma60']
 # df.loc[((dif_cond1 & dif_cond2 & dif_cond3) | (macd_cond1 & macd_cond2 & macd_cond3)) & line_cond1, 'signal_up'] = 2
 
-# RSI做多信号(6, 12, 24)
-for index, row in df.iterrows():
-    if index == 0:
-        continue
-
-    up_sum = 0
-    down_sum = 0
-    up_count = 0
-    down_count = 0
-    start_index = -1
-    # RS6
-    if index - start_index > 6:
-        start_index = index - 6
-    for ii in range(index, start_index, -1):
-        # c_index 相对区间位置索引
-        c_index = index - ii
-        if c_index == 0:
-            continue
-        try:
-            if df.at[c_index, 'close'] > df.at[c_index-1, 'close']:
-                up_sum += (df.at[c_index, 'close'] - df.at[c_index-1, 'close'])
-                up_count += 1
-            elif df.at[c_index, 'close'] < df.at[c_index-1, 'close']:
-                down_sum += (df.at[c_index-1, 'close'] - df.at[c_index, 'close'])
-                down_count += 1
-        except KeyError:
-            print('error:')
-            print(index)
-            print(ii)
-            print(c_index)
-            exit()
-    if up_sum == 0 or down_sum == 0:
-        df.at[0, 'rs6'] = 0
-    else:
-        df.at[0, 'rs6'] = (up_sum / up_count) / (down_sum / down_count)
-
-
-df['rsi6'] = 100 - (100 / (1 + df['rs6']))
-print(df)
-# print(df[['candle_begin_time', 'close', 'ma60', 'dif', 'dea', 'macd_bar', 'signal_up']])
-exit()
 
 # 做空
 dif_cond1 = df['dif'] < df['dif'].shift(1)
