5693a47524a52b56375052e556a2ad2308846534
dengjunxp <263139762@qq.com>
2020-04-03 00:48:34 +0800

    trade

---
+++ config/mongo.py
@@ -0,0 +1,7 @@
+# encoding=utf-8
+
+# mongo_host = '192.168.2.110'
+mongo_host = '192.168.10.10'
+mongo_port = '27017'
+
+
--- lib/db/mongo_handler.py
+++ lib/db/mongo_handler.py
@@ -1,12 +1,12 @@
 # encoding=utf-8
 
 from pymongo import MongoClient
+from config.mongo import mongo_host, mongo_port
 
 
 def get_mongo_handler():
-    # host = '192.168.2.110'
-    host = '192.168.10.10'
-    port = '27017'
+    host = mongo_host
+    port = mongo_port
     db_name = 'test_db'
 
     uri = 'mongodb://{host}:{port}/{db_name}?authSource={auth_source}' \
@@ -69,3 +69,39 @@ class MongoHandle:
         client = cls.get_instance()
         database = client['okex_swap']
         return database[collection_name]
+
+    @classmethod
+    def get_spot_kline(cls, instrument_id, start=0, end=0):
+        """
+        获取现货mongodb的K线数据
+        :param instrument_id
+        :param start:
+        :param end:
+        :return:
+        """
+        if start == 0 and end == 0:
+            return []
+        collection = cls.get_spot_collection(instrument_id)
+        condition = {
+            'time': {}
+        }
+
+        if start != 0:
+            condition['time']['$gte'] = start
+        if end != 0:
+            condition['time']['$lt'] = end
+        result = collection.find(condition)
+        return result
+
+    @classmethod
+    def get_swap_kline(cls, instrument_id, start=0, end=0):
+        """
+        获取合约mongodb的K线数据
+        :param instrument_id
+        :param start:
+        :param end:
+        :return:
+        """
+        collection = cls.get_swap_collection(instrument_id)
+        result = collection.find({"time": {"$gte": int(start), "$lt": int(end)}})
+        return result
